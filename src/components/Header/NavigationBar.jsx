83b06ef6a7b82b9c95737f63764ea84f2f8d7b41
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2023-09-15 07:51:55 -0500

    Change

--- src/components/Header/NavigationBar.jsx
+++ src/components/Header/NavigationBar.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import tv from '../../assets/tv.svg'
 import Search from './../Search/Search';
-import menu from '../../assets/Menu (4).svg'
+
 
 const NavigationBar = () => {
   return (
     <div className='flex flex-col md:flex-row space-y-6 lg:space-y-0 justify-between items-center w-[80%] mx-auto text-white'>
     <div className='flex items-center space-x-4'>
-        <img src={tv} alt="" />
+        <img src="tv.svg" alt="" />
         <h1 className='font-bold text-lg'>MovieBox</h1>
     </div>
     <div className='w-[100%] lg:w-[40%]'>
@@ -15,7 +14,7 @@ const NavigationBar = () => {
     </div>
     <div className='flex hidden lg:flex items-center space-x-4'>
         <h1 className='font-semibold text-normal'>Sign in</h1>
-        <img src={menu} alt="" />
+        <img src="Menu (4).svg" alt="" />
     </div>
     </div>
   )
