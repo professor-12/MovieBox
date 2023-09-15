c7b957c400cc470da87fbba26185e2dfd73ae896
Professor <135185597+professor-12@users.noreply.github.com>
2023-09-15 15:05:16 +0100

    Update Wrapper.jsx

--- src/components/Header/Wrapper/Wrapper.jsx
+++ src/components/Header/Wrapper/Wrapper.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const Wrapper = (props) => {
   return (
-    <div style = {{background: `url("public/assets/Poster(3).png")`}} className={`text-white container mx-auto h-[600px]   object-fill py-5`}>{props.children}</div>
+    <div style = {{backgroundImage: `url("assets/Poster(3).png")`}} className={`text-white container mx-auto h-[600px]   object-fill py-5`}>{props.children}</div>
   )
 }
 
