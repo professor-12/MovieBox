1504e7acea4c2f3cfab3f6176d29e3a4fe3a9d59
Professor <135185597+professor-12@users.noreply.github.com>
2023-09-15 14:45:33 +0100

    Update Wrapper.jsx

--- src/components/Header/Wrapper/Wrapper.jsx
+++ src/components/Header/Wrapper/Wrapper.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import classes from '../../../index.module.css';
-
 const Wrapper = (props) => {
   return (
-    <div  className={`text-white ${classes.container} mx-auto h-[600px]   object-fill py-5`}>{props.children}</div>
+    <div  className={`text-white container mx-auto h-[600px]   object-fill py-5`}>{props.children}</div>
   )
 }
 
